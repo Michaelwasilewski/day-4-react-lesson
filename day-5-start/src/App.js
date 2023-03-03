7536c0028f43e793545a249666ad988a7b655883
Michael Wasilewski <michael_grimestad94@hotmail.com>
2023-03-03 12:08:51 +0100

    add plain css and sass

---
+++ day-5-start/src/App.js
@@ -0,0 +1,21 @@
+import './sass/index.scss';
+
+function App() {
+	return (
+		<div>
+			<h1>Hello WOrld</h1>
+			<p>
+				lorem impsum dolor sit amet lorem impsum dolor sit
+				ametlorem impsum dolor sit ametlorem impsum dolor
+				sit amet
+			</p>
+			<ul>
+				<li>Uno</li>
+				<li>Dos</li>
+				<li>Tres</li>
+			</ul>
+		</div>
+	);
+}
+
+export default App;
