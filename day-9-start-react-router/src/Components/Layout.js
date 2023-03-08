8cba858c39363740a20555f74775b4a3f6949e8b
Michael Wasilewski <michael_grimestad94@hotmail.com>
2023-03-08 12:20:55 +0100

    day 10 start

--- day-9-start-react-router/src/Components/Layout.js
+++ day-9-start-react-router/src/Components/Layout.js
@@ -3,16 +3,6 @@ import { NavLink, Outlet } from 'react-router-dom';
 const Layout = () => {
 	return (
 		<div>
-			<nav>
-				<ul>
-					<li>
-						<NavLink to="/">Home</NavLink>
-					</li>
-					<li>
-						<NavLink to="/products">Products</NavLink>
-					</li>
-				</ul>
-			</nav>
 			<Outlet />
 			<div>I am footer</div>
 		</div>
